7a9c98f7d9b167d2b4dcb47d93609c7dcbfaae1f
Bot87Ever <vadtyasaiumesh282@gmail.com>
2026-01-11 17:15:09 +0530

    weather app with API integration

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [city, setCity] = useState("");
+  const [weather, setWeather] = useState(null);
+
+  const getWeather = async () => {
+    if (city === "") return;
+
+    const apiKey = "763e578a2a4b9415eccf186313385b0e"; // your API key
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    if (data.cod === "404") {
+      alert("City not found!");
+      return;
+    }
+
+    setWeather(data);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="container">
+        
+        <h1 className="title">Weather App</h1>
+
+        <input
+          type="text"
+          placeholder="Enter city..."
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+          onKeyDown={(e) => e.key === "Enter" && getWeather()}
+          className="search"
+        />
+
+        
+        {weather && weather.main && (
+          <div className="weather-box">
+            <h2>{weather.name}</h2>
+            <h1>{Math.round(weather.main.temp)}°C</h1>
+
+            <p>Humidity: {weather.main.humidity}%</p>
+            <p>Wind: {weather.wind.speed} km/h</p>
+            <p>{weather.weather[0].main}</p>
+          </div>
+        )}
+      </div>
+      <p className="footer"> umesh</p>
     </div>
   );
 }
